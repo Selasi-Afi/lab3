b62b6f29c3e0f31f3758500caa3a218e4025f341
Selasi <selasigborglah@gmail.com>
2017-10-20 15:01:17 +0000

    Geolocator
    Geographic location

--- js/app/tt.js
+++ js/app/tt.js
@@ -1,27 +1,25 @@
-// Since we will be making use of 3rd party functions (eg. navigator.geolocation.getCurrentPosition) which are not native javascript functions we will need
-// to add this function to the list of native javascript functions to allow javascript identify and execute it each time its called.
-// This is done by using the addEventListener() function.
-//
-console.log("loc");
-var map;
-  function initMap() {
+var map, infoWindow;
+function initMap() {
         var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
           center: uluru
         });
         }
-document.addEventListener("deviceready", onDeviceReady, false);
 
 
-//We decide to create a function to handle the 3rd party functions (eg. navigator.geolocation.getCurrentPosition)
-// which we earlier added to the native functions of the javascript
-function onDeviceReady() {
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+
+
+function button(){
+	navigator.geolocation.getCurrentPosition(onSuccess, onError);
 }
-
-// onSuccess Geolocation
-//
 
 function onSuccess(position) {
 
